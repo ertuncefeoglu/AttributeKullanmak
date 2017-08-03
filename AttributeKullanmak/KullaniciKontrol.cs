4c0a65d3dda7a40572cd388161f17100b6316f5c
Ertunç Efeoğlu <ertuncefeoglu@gmail.com>
2017-08-03 22:02:55 +0300

    Attribute kullanmak
    Attribute kullanmak

---
+++ AttributeKullanmak/KullaniciKontrol.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AttributeKullanmak
+{
+    public static class KullaniciKontrol
+    {
+        public static bool Kontrol(Kullanici kullanici)
+        {
+            var retVal = true;
+
+            var props = kullanici.GetType().GetProperties();
+
+            foreach (var propertyinfo in props)
+            {
+                if (propertyinfo.GetCustomAttributes(typeof(BosOlamazAttribute), true).Any())
+                {
+                    var val = propertyinfo.GetValue(kullanici) as string;
+                    if (string.IsNullOrEmpty(val))
+                    {
+                        retVal = false;
+                    }
+                }
+
+            }
+
+            return retVal;
+        }
+    }
+}
---
+++ AttributeKullanmak/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AttributeKullanmak
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            var kullanici = new Kullanici { Adi = "Ertunç" };
+            //var kullanici = new Kullanici { Adi = "Ertunç" };
+
+            if (KullaniciKontrol.Kontrol(kullanici))
+            {
+                Console.WriteLine("Kullanıcı doğru oluşturuldu");
+            }
+            else
+            {
+                Console.WriteLine("Kullanıcı yanlış oluşturuldu");
+            }
+
+            Console.ReadKey();
+        }
+
+    }
+}
